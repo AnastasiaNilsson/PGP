7cea811328ff417e659e9fd6d9f1471e73081d39
AnastasiaNilsson <anastasia@nilsson.pro>
2023-05-08 13:11:00 +0200

    Show name, age and address on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { User, UserDTO } from './Types.ts'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState({} as User);
+  const [userIsLoaded, setUserIsLoaded] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+
+    const fetchUser = async () => {
+      return await fetch("https://randomuser.me/api/").then(async response => {
+        const json: UserDTO = await response.json();
+        return json.results[0] as User;
+      });
+    };
+
+    fetchUser().then(result => {
+      setUser(result);
+      setUserIsLoaded(true);
+    });
+
+  }, []);
+
+  return <>
+    <header className="header">
+      <a href="https://vitejs.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <h1>React Lab 1</h1>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </header>
+
+    {
+      userIsLoaded && (
+        <main className="main">
+          <h2>{user.name.title} {user.name.first} {user.name.last} (age {user.dob.age})</h2>
+          <p>Address: {user.location.street.number} {user.location.street.name}, {user.location.postcode}, {user.location.city}, {user.location.country}</p>
+        </main>
+      )
+    }
+
+
+  </>
 }
 
 export default App
---
+++ src/Types.ts
@@ -0,0 +1,41 @@
+export type User = {
+    gender: string,
+    name: {
+        title: string,
+        first: string,
+        last: string
+    },
+    location: {
+        street: {
+          number: number,
+          name: string
+        },
+        city: string,
+        state: string,
+        country: string,
+        postcode: number,
+        coordinates: {
+          latitude: string,
+          longitude: string
+        },
+        timezone: {
+          offset: string,
+          description: string
+        }
+    },
+    email: string,
+    dob: {
+        date: string,
+        age: number
+    },
+    picture: {
+        large: string,
+        medium: string,
+        thumbnail: string
+    },
+    nat: string
+}
+
+export type UserDTO = {
+    results: User[],
+}
